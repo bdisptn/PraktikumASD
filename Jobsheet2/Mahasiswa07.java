7529da8093ccde8d7f7e77ecad2ec30d6b0fbcac
bdisptn <budisptn13@gmail.com>
2026-02-20 13:19:31 +0700

    Percobaan 1

---
+++ Jobsheet2/Mahasiswa07.java
@@ -0,0 +1,53 @@
+package Jobsheet2;
+
+public class Mahasiswa07 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa07() {
+
+    }
+
+    public Mahasiswa07(String nm, String nim, String kls, double ipk) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (0.0 >= ipk && 4.0 <= ipk) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus diantara 0.0 dan 4.0");
+        }
+       
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+
+}
+
