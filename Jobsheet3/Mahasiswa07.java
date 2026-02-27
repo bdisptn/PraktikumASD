371a3ff1b41e1019d3f1f52010941cdb0ef2d856
bdisptn <budisptn13@gmail.com>
2026-02-27 13:42:25 +0700

    Praktikum 1

---
+++ Jobsheet3/Mahasiswa07.java
@@ -0,0 +1,18 @@
+package Jobsheet3;
+
+public class Mahasiswa07 {
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
+
+    void cetakInfo() {
+        System.out.println("NIM     : "+ nim);
+        System.out.println("Nama    : "+ nama);
+        System.out.println("Kelas   : "+ kelas);
+        System.out.println("IPK     : "+ ipk);
+    }
+}
+
+
+
